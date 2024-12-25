917b4089fbff8b8db7101c366a40f472678afc10
hosoyanaomi <cppbt124@yahoo.co.jp>
2024-12-25 20:12:34 +0900

    first commit

---
+++ MyInternAfter/src/test/java/overtime/example/MyInternAfterApplicationTests.java
@@ -0,0 +1,13 @@
+package overtime.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInternAfterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
